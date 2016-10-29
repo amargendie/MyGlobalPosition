09293d7d0354ce30da577d2e30338c040e2dc4e1
Andres Margendie Benoit <machandi@gmail.com>
2016-10-29 16:41:49 +0200

    first commit

---
+++ TGFinancialBot.js
@@ -0,0 +1,25 @@
+const Telegram = require('telegram-node-bot')
+const TelegramBaseController = Telegram.TelegramBaseController
+const TextCommand = Telegram.TextCommand
+const tg = new Telegram.Telegram('AAFINIto8fY9LGKenota04sPWZ4ZA7oPYgk')
+
+class PingController extends TelegramBaseController {
+    /**
+     * @param {Scope} $
+     */
+    pingHandler($) {
+        $.sendMessage('pong')
+    }
+
+    get routes() {
+        return {
+            'pingCommand': 'pingHandler'
+        }
+    }
+}
+
+tg.router
+    .when(
+        new TextCommand('ping', 'pingCommand'),
+        new PingController()
+)
